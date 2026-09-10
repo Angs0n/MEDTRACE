8554fcc0d55eb14c3753daa13bb105d707d923a6
Halimah Oluwafunmilola Aliu <uracilcycline@gmail.com>
2026-09-10 22:02:26 +0100

    Delete MEDTRACE/tests directory

--- MEDTRACE/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Must be set before importing app.main, which creates the SQLAlchemy engine.
-os.environ["DATABASE_URL"] = "sqlite:///./test_medtrace.db"
-os.environ["JWT_SECRET"] = "test-only-secret"
